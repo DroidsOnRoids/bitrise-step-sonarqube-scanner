90522427e0d4e90ca950f44434fe216900cacd62
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2017-06-04 17:38:47 +0200

    Java runtime version detection added

--- step.sh
+++ step.sh
@@ -9,6 +9,11 @@ if [[ ! -z ${scanner_properties} ]]; then
   echo -n "${scanner_properties}" > sonar-project.properties
 fi
 
+JAVA_VERSION_MAJOR=$(java -version 2>&1 | grep -i version | sed 's/.*version ".*\.\(.*\)\..*"/\1/; 1q')
+if [ "${JAVA_VERSION_MAJOR}" -lt "8" ]; then
+  echo -e "\e[91mSonar Scanner CLI requires JRE or JDK version 8 or newer. Version \"${JAVA_VERSION_MAJOR}\" has been detected, CLI may not work properly.\e[0m"
+fi
+
 pushd $(mktemp -d)
 wget https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${scanner_version}.zip
 unzip sonar-scanner-cli-${scanner_version}.zip
