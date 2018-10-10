502ac1d864657c47b11d0654c1337a2c5dd829dd
Luciano Almeida <passos.luciano@outlook.com>
2018-10-09 21:39:57 -0300

    Update step.sh

--- step.sh
+++ step.sh
@@ -23,7 +23,7 @@ else
 fi
 
 pushd $(mktemp -d)
-wget  https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${scanner_version}.zip
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${scanner_version}.zip
 unzip sonar-scanner-cli-${scanner_version}.zip
 TEMP_DIR=$(pwd)
 popd
