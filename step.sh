8357d21ce11f9767c502948fbbf56eb253401bf1
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2021-05-19 17:30:02 +0200

    Update step.sh

--- step.sh
+++ step.sh
@@ -55,5 +55,5 @@ else
   debug_flag=""
 fi
 
-sudo dtruss ${TEMP_DIR}/sonar-scanner-${scanner_version}/bin/sonar-scanner $debug_flag
+${TEMP_DIR}/sonar-scanner-${scanner_version}/bin/sonar-scanner $debug_flag
 
